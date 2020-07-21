88cf5152a4844bdc1e0a746796487164989e6c50
richard_ivan <richardivan100499@gmail.com>
2020-07-21 15:00:39 +0700

    feat(core): add home goods list screen

---
+++ lib/Screen/goodsList.dart
@@ -0,0 +1,124 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:goods_list_item/Model/goods.dart';
+import 'package:goods_list_item/Util/database_helper.dart';
+import 'package:sqflite/sqflite.dart';
+
+class GoodsList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return GoodsListState();
+  }
+}
+
+class GoodsListState extends State<GoodsList> {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  List<Goods> goodsList;
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    if (goodsList == null) {
+      goodsList = List<Goods>();
+      updateListView();
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Barang'),
+        backgroundColor: Colors.blue.shade800,
+      ),
+      body: getGoodsListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('FAB clicked');
+          //navigateToDetail(Goods('', '', 0, '' ,''), 'Add Barang');
+        },
+        tooltip: 'Add Barang',
+        backgroundColor: Colors.blue.shade800,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  ListView getGoodsListView() {
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.amber,
+              child: Text(getFirstLetter(this.goodsList[position].name),
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
+            title: Text(this.goodsList[position].name,
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            subtitle: Text(this.goodsList[position].price.toString()),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                GestureDetector(
+                  child: Icon(Icons.delete,color: Colors.red,),
+                  onTap: () {
+                    _delete(context, goodsList[position]);
+                  },
+                ),
+              ],
+            ),
+            onTap: () {
+              debugPrint("ListTile Tapped");
+              //navigateToDetail(this.goodsList[position], 'Edit Barang');
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  getFirstLetter(String title) {
+    return title.substring(0, 2);
+  }
+
+ 
+  void _delete(BuildContext context, Goods goods) async {
+    int result = await databaseHelper.deleteGoods(goods.id);
+    if (result != 0) {
+      _showSnackBar(context, 'Barang berhasil di delete');
+      updateListView();
+    }
+  }
+
+  void _showSnackBar(BuildContext context, String message) {
+    final snackBar = SnackBar(content: Text(message));
+    Scaffold.of(context).showSnackBar(snackBar);
+  }
+
+  // void navigateToDetail(Goods goods, String title) async {
+  //   bool result =
+  //       await Navigator.push(context, MaterialPageRoute(builder: (context) {
+  //     return GoodsDetail(goods, title);
+  //   }));
+
+  //   if (result == true) {
+  //     updateListView();
+  //   }
+  // }
+
+  void updateListView() {
+    final Future<Database> dbFuture = databaseHelper.initializeDatabase();
+    dbFuture.then((database) {
+      Future<List<Goods>> goodsListFuture = databaseHelper.getGoodsList();
+      goodsListFuture.then((goodsList) {
+        setState(() {
+          this.goodsList = goodsList;
+          this.count = goodsList.length;
+        });
+      });
+    });
+  }
+
+  
+}
